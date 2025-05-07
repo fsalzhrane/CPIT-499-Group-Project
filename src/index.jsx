f511b84aa74a0235797adf73b0b3df78716e3d54
FAISAL ALZAHRANI <123291119+fsalzhrane@users.noreply.github.com>
2025-05-07 22:33:18 +0300

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -23,9 +23,3 @@ root.render(
     </QueryClientProvider>
   </React.StrictMode>
 )
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// import reportWebVitals from './reportWebVitals' // You would need to create this file if you want it
-// reportWebVitals()
